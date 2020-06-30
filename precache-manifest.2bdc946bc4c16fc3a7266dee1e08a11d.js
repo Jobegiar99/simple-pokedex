62a1fe8840310a515147f30fe29d35de4bfd18ca
José Bernardo Girón Arana <bernardo.giron99@gmail.com>
2020-06-30 11:56:16 -0600

    Updates

---
+++ precache-manifest.2bdc946bc4c16fc3a7266dee1e08a11d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c986375532e895d9bdf2f450e4f2f9",
+    "url": "/simple-pokedex/index.html"
+  },
+  {
+    "revision": "734cbf173d1727131442",
+    "url": "/simple-pokedex/static/css/main.5cccac42.chunk.css"
+  },
+  {
+    "revision": "159f8561150e7fe47c5d",
+    "url": "/simple-pokedex/static/js/2.03d26429.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/simple-pokedex/static/js/2.03d26429.chunk.js.LICENSE"
+  },
+  {
+    "revision": "734cbf173d1727131442",
+    "url": "/simple-pokedex/static/js/main.c9460b97.chunk.js"
+  },
+  {
+    "revision": "e3af63261b76da6fac81",
+    "url": "/simple-pokedex/static/js/runtime-main.28e449f7.js"
+  }
+]);
